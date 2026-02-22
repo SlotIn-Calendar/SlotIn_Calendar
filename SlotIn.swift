633e7c871a1d0ea339980726a6480689407966f4
mingee <87844547+min-gyoung@users.noreply.github.com>
2026-02-22 21:32:28 +0900

    Update and rename .gitattributes to SlotIn.swift

---
+++ SlotIn.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// SlotIn-Calendar Project
+struct SlotInCalendar {
+    let description: String = "빈 시간을 한눈에, 하루를 더 효율적으로"
+    let primaryLanguage: String = "Swift"
+}
+
+print("SlotIn-Calendar is powered by Swift.")
